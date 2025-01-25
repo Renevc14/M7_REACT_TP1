acbc211f10917640a4ff81d58531cefe00c895aa
Rene14 <Rene.vilela@ucb.edu.bo>
2025-01-25 02:13:51 -0400

    lab 5

--- src/components/Core/OpenLink.jsx
+++ src/components/Core/OpenLink.jsx
@@ -3,11 +3,11 @@ import githubLogo from '../../assets/Github.svg';
 
 const OpenLink = ({ redirectURL, logoImage }) => {
     return (
-        <div>
+        <>
             <a href={redirectURL} target="_blank">
             <img src={logoImage} className="logo" alt="Github logo" />
             </a>
-        </div>
+        </>
     );
 };
 

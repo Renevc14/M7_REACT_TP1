d2792a8131894afcc2082d82d0a18ff94927103d
Rene14 <Rene.vilela@ucb.edu.bo>
2025-01-31 15:05:55 -0400

    cicd

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/m7-react-tp1">
+      <BrowserRouter basename="/M7_REACT_TP1">
         <App />
       </BrowserRouter>
     </Provider>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/m7-react-tp1/'
+  base: '/M7_REACT_TP1/'
 })
